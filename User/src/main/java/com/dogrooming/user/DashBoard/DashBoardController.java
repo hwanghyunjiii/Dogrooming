fff3950e9a51c444a7d42f7e6fd6b80157c3434a
haram <un2577@gmail.com>
2021-12-29 21:54:12 +0900

    유저 대시보드 생성

---
+++ User/src/main/java/com/dogrooming/user/DashBoard/DashBoardController.java
@@ -0,0 +1,22 @@
+package com.dogrooming.user.DashBoard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DashBoardController {
+
+    @GetMapping(value = "/dashboard")
+    public String DashBoard(){
+        return "../view/dashboard.html";
+    }
+
+    @ResponseBody
+    @GetMapping("/dash-value")
+    public String DashBoardValueTest()
+    {
+        return "test haram!!!";
+    }
+}
