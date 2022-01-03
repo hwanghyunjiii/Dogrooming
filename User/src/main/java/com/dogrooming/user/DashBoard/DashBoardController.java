d21c4b3dacf4eccd0f07431372f33281ea439e54
haram <un2577@gmail.com>
2022-01-03 23:40:36 +0900

    layout / 부트스트랩 적용

--- User/src/main/java/com/dogrooming/user/DashBoard/DashBoardController.java
+++ User/src/main/java/com/dogrooming/user/DashBoard/DashBoardController.java
@@ -2,7 +2,6 @@ package com.dogrooming.user.DashBoard;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,7 +9,7 @@ public class DashBoardController {
 
     @GetMapping(value = "/dashboard")
     public String DashBoard(){
-        return "../templates/dashboard.html";
+        return "page/dashboard";
     }
 
     @ResponseBody
