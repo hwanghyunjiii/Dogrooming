a524eb6c04f19cbe22799951cac1e30a218149fc
hwanghyunjiii <hwaanghj@gmail.com>
2022-01-04 23:39:22 +0900

    [admin] fragments 추가

--- Admin/src/main/java/com/dogrooming/admin/admin/AdminController.java
+++ Admin/src/main/java/com/dogrooming/admin/admin/AdminController.java
@@ -6,15 +6,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
+    // 테스트 페이지 (추후 삭제 예정)
+    @GetMapping("/admin/test")
+    public String test(){
+        return "admin/test";
+    }
+
     //로그인
     @GetMapping("/admin/login")
     public String login(){
-        return "admin/login.html";
+        return "admin/login";
     }
 
     //회원가입
     @GetMapping("/admin/register")
     public String register(){
-        return "admin/register.html";
+        return "admin/register";
     }
 }
