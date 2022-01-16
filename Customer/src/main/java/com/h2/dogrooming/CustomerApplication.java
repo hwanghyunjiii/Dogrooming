edcf68fdfad19a8d9e1bb0bbc3a1bb6b6e6892ee
hwanghyunjiii <hwaanghj@gmail.com>
2022-01-16 23:50:15 +0900

    불필요한 문구 삭제

--- Customer/src/main/java/com/h2/dogrooming/CustomerApplication.java
+++ Customer/src/main/java/com/h2/dogrooming/CustomerApplication.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:Common/src/main/java/com/dogrooming/Common/MainApplication.java
-package com.dogrooming.Common;
-=======
 package com.h2.dogrooming;
->>>>>>> master:Customer/src/main/java/com/h2/dogrooming/CustomerApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
