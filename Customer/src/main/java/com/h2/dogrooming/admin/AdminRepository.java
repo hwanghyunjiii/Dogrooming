db9865312286e1c562e0a7e80bfa2942c6c6efd8
haram <un2577@gmail.com>
2022-02-15 22:13:45 +0900

    footer, header 디자인 수정

--- Customer/src/main/java/com/h2/dogrooming/admin/AdminRepository.java
+++ Customer/src/main/java/com/h2/dogrooming/admin/AdminRepository.java
@@ -12,9 +12,10 @@ public interface AdminRepository extends JpaRepository<Admin, String> {
 
     boolean existsByAdminId(String adminId);
 
-    Optional<Admin> findByAdminId(String adminId);
+    Optional<Admin> findByAdminIdAndType(String adminId, Integer type);
 
     Admin getAdminByAdminId(String adminId);
 
     Admin findAdminByNameAndEmail(String name, String email);
+
 }
--- Customer/src/main/java/com/h2/dogrooming/config/SecurityConfig.java
+++ Customer/src/main/java/com/h2/dogrooming/config/SecurityConfig.java
@@ -35,11 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-
-
         http.authorizeRequests()
                 // 페이지 권한 설정
-                .antMatchers("/admin/**").hasRole("CUSTOMER")
                 .antMatchers("/**").permitAll()
                 .and() // 로그인 설정
                 .formLogin()
